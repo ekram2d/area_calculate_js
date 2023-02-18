f45647e361a857b7c324620e8638d2d5e4ff42df
Ekram Hossain <ekramhossain117@gmail.com>
2023-02-18 09:01:06 +0600

    navbar create

---
+++ js/geometry.js
@@ -0,0 +1,35 @@
+
+document.getElementById("nav-btn").addEventListener("click",function(){
+      console.log(1);
+      window.location.href="question.html";
+
+})
+
+
+
+//  document.getElementById("background").addEventListener("mous",function(){
+     
+         
+//     setBackgroundColor();
+          
+          
+//  })
+//  const getRandomNumber = (maxNum) => {
+//       return Math.floor(Math.random() * maxNum);
+//     };
+//  const getRandomColor = () => {
+//       const h = getRandomNumber(360);
+//       const s = getRandomNumber(100);
+//       const l = getRandomNumber(100);
+//       return `hsl(${h}deg, ${s}%, ${l}%)`;
+//     };
+   
+//     setInterval(() => {
+//       setBackgroundColor();
+//     }, 1500);
+    
+//     const setBackgroundColor = () => {
+//       const randomColor = getRandomColor();
+//       background.style.backgroundColor = randomColor;
+//       background.style.color = randomColor;
+//     };
