84df809c92c0d12812f4e6d6c4eac43fa0374670
Ekram Hossain <ekramhossain117@gmail.com>
2023-02-18 10:21:29 +0600

    getCardName function created

--- js/geometry.js
+++ js/geometry.js
@@ -1,9 +1,90 @@
-
+const tableContainer = document.getElementById("table-container");
+let count = 0 ;
 document.getElementById("nav-btn").addEventListener("click",function(){
-      console.log(1);
+     // console.log(1);
       window.location.href="question.html";
 
 })
+
+
+function getCardName(parentNode) {
+      let Name = parentNode.childNodes[1].innerText ;
+      return Name;
+
+}
+
+function getCardInputvalue(parentNode,index1,index2) {
+      let InputValue = parentNode.childNodes[index1].childNodes[index2].value;
+      return InputValue;
+
+
+}
+
+function createTr(tr ,count,Name,firstInput,secondInput,total){
+      tr.innerHTML = `
+      <td>${count} .</td>
+                                       <td>${Name}</td>
+                                       <td>${total} cm<sup>2</sup></td>
+                                       <td><button class="btn btn-primary">Convert to m<sup>2</sup></button></td>
+      
+      
+      `
+      return tr;
+
+}
+document.getElementById("triangle-btn").addEventListener("click",function(e){
+      let parentNode = e.target.parentNode.parentNode;
+     
+      let Name =getCardName(parentNode);
+      
+      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
+      let total = (0.5 * firstInput * secondInput).toFixed(2) ;
+      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+            alert("please enter the positive number");
+            return ;
+      }
+      else{
+
+            count+=1;
+
+   const tr=document.createElement("tr");
+   createTr(tr,count, Name,firstInput,secondInput,total);
+ 
+      console.log(tr);
+
+      tableContainer.appendChild(tr);
+      }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
