89db4df65e40cf05bea97a45994b22151a8b735f
Ekram Hossain <ekramhossain117@gmail.com>
2023-02-18 12:33:30 +0600

    rearange the code

--- js/geometry.js
+++ js/geometry.js
@@ -1,26 +1,30 @@
 const tableContainer = document.getElementById("table-container");
-let count = 0 ;
-document.getElementById("nav-btn").addEventListener("click",function(){
-     //get console.log(1);
-      window.location.href="question.html";
+let count = 0;
+
+//create link to the answer file
+document.getElementById("nav-btn").addEventListener("click", function () {
+
+      window.location.href = "question.html";
 
 })
 
 
 function getCardName(parentNode) {
-      let Name = parentNode.childNodes[1].innerText ;
+      let Name = parentNode.childNodes[1].innerText;
       return Name;
 
 }
 
-function getCardInputvalue(parentNode,index1,index2) {
+
+// get input value function 
+function getCardInputvalue(parentNode, index1, index2) {
       let InputValue = parentNode.childNodes[index1].childNodes[index2].value;
       return InputValue;
 
 
 }
 // tr created by function 
-function createTr(tr ,count,Name,firstInput,secondInput,total){
+function createTr(tr, count, Name, firstInput, secondInput, total) {
       tr.innerHTML = `
       <td>${count} .</td>
                                        <td>${Name}</td>
@@ -32,54 +36,58 @@ function createTr(tr ,count,Name,firstInput,secondInput,total){
       return tr;
 
 }
+
+
+
+
 // trianlge addeventLisiener
-document.getElementById("triangle-btn").addEventListener("click",function(e){
+document.getElementById("triangle-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
-     
-      let Name =getCardName(parentNode);
-      
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (0.5 * firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let Name = getCardName(parentNode);
+
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (0.5 * firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   createTr(tr,count, Name,firstInput,secondInput,total);
- // console.log(tr);
- tr.classList.add("mb-4");
-      tableContainer.appendChild(tr);
+            const tr = document.createElement("tr");
+            createTr(tr, count, Name, firstInput, secondInput, total);
+
+            tr.classList.add("mb-4");
+            tableContainer.appendChild(tr);
       }
 })
 
 // rectangle addeventLitener 
-document.getElementById("rectanlge-btn").addEventListener("click",function(e){
+document.getElementById("rectanlge-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
+
+      let Name = getCardName(parentNode);
      
-      let Name =getCardName(parentNode);
-      //console.log(1);
-      
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   createTr(tr,count, Name,firstInput,secondInput,total);
- // console.log(tr);
- tr.classList.add("mb-4");
-      tableContainer.appendChild(tr);
+            const tr = document.createElement("tr");
+            createTr(tr, count, Name, firstInput, secondInput, total);
+            
+            tr.classList.add("mb-4");
+            tableContainer.appendChild(tr);
       }
 })
 
@@ -90,29 +98,27 @@ document.getElementById("rectanlge-btn").addEventListener("click",function(e){
 
 
 
-document.getElementById("parallelogram-btn").addEventListener("click",function(e){
+document.getElementById("parallelogram-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
-     
-      let Name =getCardName(parentNode);
-      console.log(1);
-      
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let Name = getCardName(parentNode);
+    
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   createTr(tr,count, Name,firstInput,secondInput,total);
-   tr.classList.add("mb-4");
- // console.log(tr);
-
-      tableContainer.appendChild(tr);
+            const tr = document.createElement("tr");
+            createTr(tr, count, Name, firstInput, secondInput, total);
+            tr.classList.add("mb-4");
+           
+            tableContainer.appendChild(tr);
       }
 })
 
@@ -130,29 +136,29 @@ document.getElementById("parallelogram-btn").addEventListener("click",function(e
 
 
 
-document.getElementById("rhombus-btn").addEventListener("click",function(e){
+document.getElementById("rhombus-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
+
+      let Name = getCardName(parentNode);
      
-      let Name =getCardName(parentNode);
-      console.log(1);
-      
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (0.5*firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (0.5 * firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   createTr(tr,count, Name,firstInput,secondInput,total);
-   tr.classList.add("mb-4");
- // console.log(tr);
+            const tr = document.createElement("tr");
+            createTr(tr, count, Name, firstInput, secondInput, total);
+            tr.classList.add("mb-4");
+            
 
-      tableContainer.appendChild(tr);
+            tableContainer.appendChild(tr);
       }
 })
 
@@ -172,29 +178,29 @@ document.getElementById("rhombus-btn").addEventListener("click",function(e){
 
 
 
-document.getElementById("pentagon-btn").addEventListener("click",function(e){
+document.getElementById("pentagon-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
+
+      let Name = getCardName(parentNode);
      
-      let Name =getCardName(parentNode);
-      console.log(1);
-      
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (0.5*firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (0.5 * firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   createTr(tr,count, Name,firstInput,secondInput,total);
-   tr.classList.add("mb-4");
- // console.log(tr);
+            const tr = document.createElement("tr");
+            createTr(tr, count, Name, firstInput, secondInput, total);
+            tr.classList.add("mb-4");
+           
 
-      tableContainer.appendChild(tr);
+            tableContainer.appendChild(tr);
       }
 })
 
@@ -207,68 +213,38 @@ document.getElementById("pentagon-btn").addEventListener("click",function(e){
 
 
 
-document.getElementById("ellipse-btn").addEventListener("click",function(e){
+document.getElementById("ellipse-btn").addEventListener("click", function (e) {
       let parentNode = e.target.parentNode.parentNode;
-     
-      let Name =getCardName(parentNode);
-      console.log(1);
+
+      let Name = getCardName(parentNode);
       
-      let firstInput =parseFloat(getCardInputvalue(parentNode,5,1));
-      let secondInput = parseFloat(getCardInputvalue(parentNode,5,3));
-      let total = (3.14*firstInput * secondInput).toFixed(2) ;
-      if(isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0 ){
+
+      let firstInput = parseFloat(getCardInputvalue(parentNode, 5, 1));
+      let secondInput = parseFloat(getCardInputvalue(parentNode, 5, 3));
+      let total = (3.14 * firstInput * secondInput).toFixed(2);
+      if (isNaN(firstInput) || isNaN(secondInput) || firstInput <= 0 || secondInput <= 0) {
             alert("please enter the positive number");
-            return ;
+            return;
       }
-      else{
+      else {
 
-            count+=1;
+            count += 1;
 
-   const tr=document.createElement("tr");
-   
-   createTr(tr,count, Name,firstInput,secondInput,total);
-   
- // console.log(tr);
- tr.classList.add("mb-2");
- tr.classList.add("overflow-scroll");
+            const tr = document.createElement("tr");
 
-      tableContainer.appendChild(tr);
+            createTr(tr, count, Name, firstInput, secondInput, total);
+
+            
+            tr.classList.add("mb-2");
+            tr.classList.add("overflow-scroll");
+
+            tableContainer.appendChild(tr);
       }
 })
+document.que
 
-
-randomColor = function(e) {
-	e.style.background =  "#" + (Math.random()* 0xFFFFFF<<0).toString(16);
+randomColor = function (e) {
+      e.style.background = "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
 }
 
 
-
-
-
-
-//  document.getElementById("background").addEventListener("mous",function(){
-     
-         
-//     setBackgroundColor();
-          
-          
-//  })
-//  const getRandomNumber = (maxNum) => {
-//       return Math.floor(Math.random() * maxNum);
-//     };
-//  const getRandomColor = () => {
-//       const h = getRandomNumber(360);
-//       const s = getRandomNumber(100);
-//       const l = getRandomNumber(100);
-//       return `hsl(${h}deg, ${s}%, ${l}%)`;
-//     };
-   
-//     setInterval(() => {
-//       setBackgroundColor();
-//     }, 1500);
-    
-//     const setBackgroundColor = () => {
-//       const randomColor = getRandomColor();
-//       background.style.backgroundColor = randomColor;
-//       background.style.color = randomColor;
-//     };
